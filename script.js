d3e48ddcfc42f96a68165ee2db71198297ca4516
srn <vishoujo@gmail.com>
2026-03-10 12:42:29 +0900

    Change the sensor used

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 //id="start" のボタン要素を取得
-const button = document.getElementById("start");
+const startBut = document.getElementById("start");
 
 //ボタンが押されたら中の処理を実行
-button.addEventListener("click", async () => {
-    //iPhone のセンサー使用許可を取る
-    if (DeviceOrientationEvent.requestPermission) {
-        //アクセス許可するか聞く
-        //await → 許可されるまで待つ
-        const res = await DeviceOrientationEvent.requestPermission();
-
-        //許可されなかったら許可取るダイアログ出す
-        if (res === "granted") {
-            window.addEventListener("deviceorientation", e => {
-                console.log(e.alpha, e.beta, e.gamma);
-            });
-        }
+startBut.addEventListener("click", async () => {
+    if (typeof DeviceOrientationEvent.requestPermission === "function") {
+        //アクセス許可されるまで待つ
+        const res = await DeviceMotionEvent.requestPermission();
+        //許可なければ終了
+        if (res !== "granted") return ;
     }
 
     //センサーイベントを取得し、表示する
-    window.addEventListener("deviceorientation", (event) => {
-        //HTML id="指定のid" のタグの中身を書き換える
-        document.getElementById("alpha").textContent = "alpha : " + event.alpha;
-        document.getElementById("beta").textContent = "beta : " + event.beta;
-        document.getElementById("gamma").textContent = "gamma : " + event.gamma;
-    });
+    window.addEventListener("devicemotion", viewAcc);
 });
+
+/**
+ * センサーの値を表示する
+ * @param {*} event 
+ * @returns 
+ */
+function viewAcc(event) {
+    const acc = event.accelerationIncludingGravity;
+    if (!acc) return;
+
+    //HTML id="指定のid" のタグの中身を書き換える
+    document.getElementById("x").textContent = "x : " + acc.x;
+    document.getElementById("y").textContent = "y : " + acc.y;
+    document.getElementById("z").textContent = "z : " + acc.z;
+}
--- shake.js
+++ shake.js
@@ -2,42 +2,37 @@
 
 let shakeCount = 0;
 let lastShake = 0;
-const SHAKE_THRESHOLD = 1000000; //このベクトルの大きさを超えた時 1 カウントとする
-const COOLDOWN = 20;       //500ms 間隔で 1 回カウント
+const SHAKE_THRESHOLD = 20; //このベクトルの大きさを超えた時 1 カウントとする
+const COOLDOWN = 500;       //500ms 間隔で 1 回カウント
 
 //id="shake" ボタン要素取得
 const shakeBtn = document.getElementById('shake');
 //押されたら
 shakeBtn.addEventListener('click', async () => {
-    if (DeviceOrientationEvent.requestPermission) {
+    if (typeof DeviceOrientationEvent.requestPermission === "function") {
         //アクセス許可されるまで待つ
-        const res = await DeviceOrientationEvent.requestPermission();
-
-        if (res === "granted") {
-            window.addEventListener("deviceorientation", event => {
-                console.log(event.alpha, event.beta, event.gamma);
-            })
-        }
+        const res = await DeviceMotionEvent.requestPermission();
+        //許可なければ終了
+        if (res !== "granted") return ;
     }
 
-    window.addEventListener("deviceorientation", (event) => {
-        handleMotion(event);
+    //モーションイベント開始
+    window.addEventListener("devicemotion", handleMotion);
 
-        shakeBtn.disabled = true;
-        shakeBtn.innerText = '計測中...';
-    })
-
+    //ボタン無効化
+    shakeBtn.disabled = true;
+    shakeBtn.innerText = '計測中...';
 });
 
 function handleMotion(event) {
-    const gyro = event.rotationRate;
-    if (!gyro) return;
+    const acc = event.accelerationIncludingGravity;
+    if (!acc) return;
 
-    const a = gyro.alpha || 0;
-    const b = gyro.beta || 0;
-    const g = gyro.gamma || 0;
+    const x = acc.x || 0;
+    const y = acc.y || 0;
+    const z = acc.z || 0;
     //3 方向ベクトルを合成
-    const totalRotation = Math.sqrt(a*a + b*b + c*c);
+    const totalRotation = Math.sqrt(x*x + y*y + z*z);
     const now = Date.now();
 
     //ベクトル値が閾値を超えた && 前回の値取得から 500ms の時間経った
