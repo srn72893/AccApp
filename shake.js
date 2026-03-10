26b58bd67c4a3b58e93a849e2b592361765231a2
srn <vishoujo@gmail.com>
2026-03-10 17:59:42 +0900

    Fine adjustment

--- shake.js
+++ shake.js
@@ -1,9 +1,4 @@
-//TODO : 現状傾けただけでもカウントが進む。センサー値のノイズ除去必要
 
-let shakeCount = 0;
-let lastShake = 0;
-const SHAKE_THRESHOLD = 20; //このベクトルの大きさを超えた時 1 カウントとする
-const COOLDOWN = 500;       //500ms 間隔で 1 回カウント
 
 //id="shake" ボタン要素取得
 const shakeBtn = document.getElementById('shake');
@@ -24,15 +19,26 @@ shakeBtn.addEventListener('click', async () => {
     shakeBtn.innerText = '計測中...';
 });
 
+/**
+ * 加速度センサー値からスマホを振ったかどうか判定
+ * @param {*} event 
+ * @returns 
+ */
 function handleMotion(event) {
+    let shakeCount = 0;         //スマホを振った回数
+    let lastShake = 0;          //前回スマホを振った時間
+    const SHAKE_THRESHOLD = 25; //閾値（超えた時 1 カウントとする）
+    const COOLDOWN = 500;       //500ms 間隔で 1 回カウント計測
+
     const acc = event.accelerationIncludingGravity;
     if (!acc) return;
 
     const x = acc.x || 0;
     const y = acc.y || 0;
     const z = acc.z || 0;
-    //3 方向ベクトルを合成
+    //x軸 y軸 z軸 の値を合成しひとつの加速度として扱う
     const totalAcceleration = Math.sqrt(x*x + y*y + z*z);
+
     const now = Date.now();
 
     //ベクトル値が閾値を超えた && 前回の値取得から 500ms の時間経った
