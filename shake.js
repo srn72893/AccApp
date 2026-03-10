8a51996478a791aea4c9efd369bfbf78a19c302b
srn <vishoujo@gmail.com>
2026-03-10 12:13:11 +0900

    Added a dialog to allow access to sensors when shaking.

--- shake.js
+++ shake.js
@@ -2,33 +2,48 @@
 
 let shakeCount = 0;
 let lastShake = 0;
-const SHAKE_THRESHOLD = 1000000000; //このベクトルの大きさを超えた時 1 カウントとする
-const COOLDOWN = 500;       //500ms 間隔で 1 回カウント
+const SHAKE_THRESHOLD = 1000000; //このベクトルの大きさを超えた時 1 カウントとする
+const COOLDOWN = 20;       //500ms 間隔で 1 回カウント
 
 //id="shake" ボタン要素取得
 const shakeBtn = document.getElementById('shake');
 //押されたら
-shakeBtn.addEventListener('click', () => {
-    window.addEventListener('devicemotion', (event) => {
-    const gyro = event.rotationRate;
-    const now = Date.now();
+shakeBtn.addEventListener('click', async () => {
+    if (DeviceOrientationEvent.requestPermission) {
+        //アクセス許可されるまで待つ
+        const res = await DeviceOrientationEvent.requestPermission();
 
+        if (res === "granted") {
+            window.addEventListener("deviceorientation", event => {
+                console.log(event.alpha, event.beta, event.gamma);
+            })
+        }
+    }
+
+    window.addEventListener("deviceorientation", (event) => {
+        handleMotion(event);
+
+        shakeBtn.disabled = true;
+        shakeBtn.innerText = '計測中...';
+    })
+
+});
+
+function handleMotion(event) {
+    const gyro = event.rotationRate;
     if (!gyro) return;
 
+    const a = gyro.alpha || 0;
+    const b = gyro.beta || 0;
+    const g = gyro.gamma || 0;
     //3 方向ベクトルを合成
-    const totalRotation = Math.sqrt(
-        gyro.alpha * gyro.alpha +
-        gyro.beta * gyro.beta +
-        gyro.gamma * gyro.gamma
-    );
+    const totalRotation = Math.sqrt(a*a + b*b + c*c);
+    const now = Date.now();
 
     //ベクトル値が閾値を超えた && 前回の値取得から 500ms の時間経った
     if (totalRotation > SHAKE_THRESHOLD && (now - lastShake) > COOLDOWN) {
         shakeCount++;
+        lastShake = now;
         document.getElementById('count').innerText = shakeCount;
     }
-});
-
-shakeBtn.disabled = true;
-shakeBtn.innerText = '計測中...';
-});
+}
